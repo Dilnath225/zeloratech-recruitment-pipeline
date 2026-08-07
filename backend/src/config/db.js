fa3e62d79d7a1359b648659b409a97e123c4e920
Dilnath225 <sunerarathnayaka@gmail.com>
2026-08-07 20:14:48 +0530

    initiallizing  candidates table with schema anddefault values

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,5 +1,7 @@
 const { pool} =  require('pg');
 
+//add the postgresql db connection pool configuration
+
 const pol = new pool({
     user: 'postgres',
     host: 'localhost',
@@ -8,3 +10,27 @@ const pol = new pool({
     port: 5432,
 });
 
+//initiallizing  candidates table with schema anddefault values
+
+const initDB =  async() => {
+    const createTableQuery = `
+    CREATE TABLE IF NOT EXISTS candidates (
+        id serial PRIMARY KEY,
+        name VARCHAR(100) NOT NULL,
+        application_stage VARCHAR(50) DEFAULT 'Applying Period',
+        application_date DATE DEFAULT CURRENT_DATE,
+        overall_score INT DEFAULT 0,
+        referral_status VARCHAR 10 DEFAULT 'No',
+        assessment_status VARCHAR 20 DEFAULT 'Pending',
+
+);
+    `;
+
+    try {
+        await pool.query(createTableQuery);
+        console.log("Database table checked/created succesfully.");
+
+    } catch (err) {
+        console.error("Error creating candidates table:", err);
+    }
+}
