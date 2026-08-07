d10e833e72cc2da39aee3ad933cb9f8a4ad9f66a
Dilnath225 <sunerarathnayaka@gmail.com>
2026-08-07 21:58:46 +0530

    function to add a new candidate to the database

--- backend/src/controllers/candidatecontroller.js
+++ backend/src/controllers/candidatecontroller.js
@@ -19,3 +19,32 @@ exports.getCandidates = async (req, res) => {
         res.status(500).send('Server Error');
     }
 };
+
+//add a new candidate to the database
+exports.addCandidate = async (req, res) => {
+    try {
+    const { name, application_stage, overall_score, referral_status, assessment_status } = req.body;
+
+    const query = `
+        INSERT INTO candidates (name, application_stage, overall_score, referral_status, assessment_status)
+        VALUES ($1, $2,COALESCE($3,CURRENT_DATE), $4, $5)
+        RETURNING *;
+    `;
+    
+    const values = [
+        name,
+        application_stage || 'Applying Period',
+        application_date || null,
+        overall_score || 0,
+        referral_status || 'No',
+        assessment_status || 'Pending'  
+    ];
+
+    const newCandidate = await db.query(query, values);
+    res.status(201).json({ message: 'Candidate added successfully', candidate: newCandidate.rows[0] });
+}catch (err) {
+    console.error(err.message);
+    res.status(500).json({ error: 'Server Error' });
+
+}
+};
